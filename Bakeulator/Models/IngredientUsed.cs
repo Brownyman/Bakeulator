21e92d35995e380897837884a716daa4b2f041e1
Adam Brown <adam.brown@tapiit.com>
2020-11-01 23:32:53 +0000

    fixed formatting

--- Bakeulator/Models/IngredientUsed.cs
+++ Bakeulator/Models/IngredientUsed.cs
@@ -5,17 +5,24 @@ using System.Threading.Tasks;
 
 namespace Bakeulator.Models
 {
-	public class 
-
+	public class IngredientUsed
 	{
 		public IngredientUsed(Ingredient ingredient, double amountUsed)
 		{
 			Ingredient = ingredient;
 			AmountUsed = amountUsed;
+			UsageCost = CalcCost(Ingredient.Cost, Ingredient.Quantity, AmountUsed);
 		}
 
 		public Ingredient Ingredient { get; set; }
 		public double AmountUsed { get; set; }
 		public double UsageCost { get; set; }
+
+		private double CalcCost(double cost, double amount, double used)
+		{
+			var percentage = (used / amount);
+			var result = (cost * percentage);
+			return result;
+		}
 	}
 }
